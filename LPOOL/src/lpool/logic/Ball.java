1e7ff26b8c1a535817aec78bbba2c65de1eb97fb
gtugablue <gtugablue@gmail.com>
2015-04-13 01:12:26 +0100

    Stuff.

--- LPOOL/src/lpool/logic/Ball.java
+++ LPOOL/src/lpool/logic/Ball.java
@@ -1,10 +1,30 @@
 package lpool.logic;
 
 public class Ball {
-	Vector2D pos;
+	static private final double drag = 0.01;
+	private int number;
 	
-	public Ball(Vector2D pos) {
+	private Vector2D pos;
+	private Vector2D velocity;
+	
+	public Ball(Vector2D pos, int number) {
 		this.pos = pos;
+		this.velocity = new Vector2D(100, 0);
+		this.number = number;
+	}
+	
+	public void tick(double t)
+	{
+		pos = pos.add(velocity.scale(t));
+		velocity = velocity.scale(1 - drag);
+	}
+	
+	public Vector2D getPos()
+	{
+		return pos;
 	}
 
+	public int getNumber() {
+		return number;
+	}
 }
