94babb6d08d2298ac6c1ef8fbaf3a8d2ade77c9d
aslmbc13 <asl_mbc_13@hotmail.com>
2015-05-01 00:28:52 +0100

    Started interface for QR generation on Desktop

--- LPOOL Server-core/src/lpool/gdx/GameProject.java
+++ LPOOL Server-core/src/lpool/gdx/GameProject.java
@@ -1,11 +1,17 @@
 package lpool.gdx;
 
+import java.awt.image.BufferedImage;
+import java.net.Inet4Address;
+
+import lpool.zxing.QRGenerator;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GameProject extends Game {
+public class GameProject extends Game {	
 	SpriteBatch batch;
 	Texture img;
 	int width;
@@ -13,11 +19,33 @@ public class GameProject extends Game {
 	
 	@Override
 	public void create () {
+		
+		String ip_address = "";
+		
+		try {
+			ip_address = Inet4Address.getLocalHost().getHostAddress();
+		} catch(Exception e) {
+			System.out.println("Unable to get IP address");
+		}
+		
+		BufferedImage qr_code = null;
+		
+		if(ip_address != "")
+			qr_code = QRGenerator.generateFromString("");
+		
 		width = Gdx.graphics.getDesktopDisplayMode().width;
 		height = Gdx.graphics.getDesktopDisplayMode().height;
 		//Gdx.graphics.setDisplayMode(width, height, false);
 		
 		batch = new SpriteBatch();
+		
+		if(qr_code == null)
+			System.out.println("Unable to generate QR code");
+		else
+		{
+			
+		}
+		
 		
 		setScreen(new MatchScene(width, height));
 	}
---
+++ LPOOL Server-core/src/lpool/zxing/QRGenerator.java
@@ -0,0 +1,52 @@
+package lpool.zxing;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.Hashtable;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class QRGenerator {
+
+	public static BufferedImage generateFromString(String text) {
+		
+		int size = 125;
+		try {
+			Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<EncodeHintType, ErrorCorrectionLevel>();
+			hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+			QRCodeWriter qrCodeWriter = new QRCodeWriter();
+			BitMatrix byteMatrix = qrCodeWriter.encode(text,BarcodeFormat.QR_CODE, size, size, hintMap);
+			int CrunchifyWidth = byteMatrix.getWidth();
+			BufferedImage image = new BufferedImage(CrunchifyWidth, CrunchifyWidth,
+					BufferedImage.TYPE_INT_RGB);
+			image.createGraphics();
+
+			Graphics2D graphics = (Graphics2D) image.getGraphics();
+			graphics.setColor(Color.WHITE);
+			graphics.fillRect(0, 0, CrunchifyWidth, CrunchifyWidth);
+			graphics.setColor(Color.BLACK);
+
+			for (int i = 0; i < CrunchifyWidth; i++) {
+				for (int j = 0; j < CrunchifyWidth; j++) {
+					if (byteMatrix.get(i, j)) {
+						graphics.fillRect(i, j, 1, 1);
+					}
+				}
+			}
+
+			return image;
+				
+		} catch (WriterException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}  
+
+}
