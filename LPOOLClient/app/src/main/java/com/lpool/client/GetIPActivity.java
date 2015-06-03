4505aa09a8b206792725c89a657232985cf43ce6
aslmbc13 <asl_mbc_13@hotmail.com>
2015-06-03 17:11:50 +0100

    Added overflow menus (partially)

--- LPOOLClient/app/src/main/java/com/lpool/client/GetIPActivity.java
+++ LPOOLClient/app/src/main/java/com/lpool/client/GetIPActivity.java
@@ -139,13 +139,13 @@ public class GetIPActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "Settings are currently unavailable", Toast.LENGTH_LONG).show();
+            return true;
+        } else if(id == R.id.action_instructions) {
+            testAbout(null);
             return true;
         }
 
